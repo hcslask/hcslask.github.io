db1cab785a15640082a63f51bade79b0eb05d6b4
christian <christian@nedra>
2024-11-23 23:00:38 +0100

    second commit

---
+++ game.js
@@ -0,0 +1,61 @@
+var Game = {
+    init: function() {}
+}
+
+var Game = {
+    display: null,
+
+    init: function() {
+        this.display = new ROT.Display();
+        document.body.appendChild(this.display.getContainer());
+    }
+}
+
+Game.map = {};
+Game._generateMap = function() {
+    var digger = new ROT.Map.Digger();
+
+    var digCallback = function(x, y, value) {
+        if (value) { return; } /* do not store walls */
+
+        var key = x+","+y;
+        this.map[key] = ".";
+    }
+    digger.create(digCallback.bind(this));
+}
+
+
+Game._drawWholeMap = function() {
+    for (var key in this.map) {
+        var parts = key.split(",");
+        var x = parseInt(parts[0]);
+        var y = parseInt(parts[1]);
+        this.display.draw(x, y, this.map[key]);
+    }
+}
+
+Game._generateMap = function() {
+    var digger = new ROT.Map.Digger();
+    var freeCells = [];
+
+    var digCallback = function(x, y, value) {
+        if (value) { return; } /* do not store walls */
+
+        var key = x+","+y;
+        freeCells.push(key);
+        this.map[key] = ".";
+    }
+    digger.create(digCallback.bind(this));
+
+    this._generateBoxes(freeCells);
+
+    this._drawWholeMap();
+};
+
+Game._generateBoxes = function(freeCells) {
+    for (var i=0;i<10;i++) {
+        var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+        var key = freeCells.splice(index, 1)[0];
+        this.map[key] = "*";
+    }
+};
